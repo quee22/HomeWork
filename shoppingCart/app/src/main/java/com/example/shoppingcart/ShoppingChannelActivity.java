cf80db12586a1e4e8551dfe57cae440f66b6524c
quee22 <1514545874@qq.com>
2020-11-20 16:49:07 +0800

    第七，八次作业

--- shoppingCart/app/src/main/java/com/example/shoppingcart/ShoppingChannelActivity.java
+++ shoppingCart/app/src/main/java/com/example/shoppingcart/ShoppingChannelActivity.java
@@ -46,7 +46,7 @@ public class ShoppingChannelActivity extends AppCompatActivity implements View.O
         // 从布局视图中获取名叫gv_channel的网格视图
         gv_channel = findViewById(R.id.gv_channel);
         findViewById(R.id.iv_cart).setOnClickListener(this);
-        tv_title.setText("手机商场");
+        tv_title.setText("购物商场");
     }
 
     @Override
